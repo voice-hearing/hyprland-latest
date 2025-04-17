759c93a759047efb337e0cdc1c96de14cb4a5083
Faust Magno <neurochange@proton.me>
2025-04-17 21:22:08 +0100

    Update docker.sh

--- files/scripts/docker.sh
+++ files/scripts/docker.sh
@@ -16,12 +16,9 @@ gpgcheck=1
 gpgkey=https://download.docker.com/linux/fedora/gpg
 EOF
 
-$DNF install -y \
-    containerd.io \
-    docker-buildx-plugin \
-    docker-ce \
-    docker-ce-cli \
-    docker-compose-plugin
+if [[ $FEDORA_MAJOR_VERSION -eq 42 ]]; then
+    dnf install -y --enablerepo=docker-ce-testing docker-buildx-plugin docker-ce docker-ce-cli docker-compose-plugin
+fi
 
 # prefer to have docker-compose available for legacy muscle-memory
 ln -s /usr/libexec/docker/cli-plugins/docker-compose /usr/bin/docker-compose
@@ -32,7 +29,3 @@ echo "net.ipv4.ip_forward = 1" >/usr/lib/sysctl.d/docker-ce.conf
 
 # disable repo
 sed -i "s@enabled=1@enabled=0@" /etc/yum.repos.d/docker-ce.repo
-
-if [[ $FEDORA_MAJOR_VERSION -eq 42 ]]; then
-    dnf install -y --enablerepo=docker-ce-testing docker-buildx-plugin docker-ce docker-ce-cli docker-compose-plugin
-fi
