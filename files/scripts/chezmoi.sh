ee6a7b87732fee1720d15e51f4898c0a300979de
Faust Magno <neurochange@proton.me>
2025-04-18 10:07:19 +0100

    Update chezmoi.sh

--- files/scripts/chezmoi.sh
+++ files/scripts/chezmoi.sh
@@ -1,2 +1,16 @@
-  rpm-ostree install \
-      https://github.com/twpayne/chezmoi/releases/download/v2.61.0/chezmoi-2.61.0-x86_64.rpm
+#!/usr/bin/bash
+
+set -ouex pipefail
+
+# github direct installs
+/ctx/build_files/github-release-install.sh twpayne/chezmoi x86_64
+
+# Starship Shell Prompt
+curl -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
+tar -xzf /tmp/starship.tar.gz -C /tmp
+install -c -m 0755 /tmp/starship /usr/bin
+# shellcheck disable=SC2016
+echo 'eval "$(starship init bash)"' >> /etc/bashrc
+
+# Bash Prexec
+curl -Lo /usr/share/bash-prexec https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec.sh
