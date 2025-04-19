681dab0c152750db55a25548a204f62de3d64cda
Faust Magno <neurochange@proton.me>
2025-04-19 14:07:11 +0100

    Update tailscale.sh

--- files/scripts/tailscale.sh
+++ files/scripts/tailscale.sh
@@ -22,33 +22,11 @@ echo "Installing Taiscale"
 # mkdir -p /var/lib/alternatives
 
 # Setup repo
-cat << EOF > /etc/yum.repos.d/tailscale.repo
-[tailscale-stable]
-name=Tailscale stable
-baseurl=https://pkgs.tailscale.com/stable/fedora/$basearch
-enabled=1
-type=rpm
-repo_gpgcheck=0
-gpgcheck=0
-gpgkey=https://pkgs.tailscale.com/stable/fedora/repo.gpg
-EOF
-
-# Import signing key
-# rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-
-# Now let's install the packages.
-rpm-ostree install tailscale
+sudo curl -s https://pkgs.tailscale.com/stable/fedora/tailscale.repo -o /etc/yum.repos.d/tailscale.repo
+sudo sed -i 's/repo_gpgcheck=1/repo_gpgcheck=0/' /etc/yum.repos.d/tailscale.repo
+sudo rpm-ostree install --apply-live tailscale
 
 sudo systemctl enable --now tailscaled
 # Clean up the yum repo (updates are baked into new images)
 # rm /etc/yum.repos.d/tailscale.repo -f
 
-# And then we do the hacky dance!
-# mv /var/opt/brave.com /usr/lib/brave.com # move this over here
-
-#####
-# Register path symlink
-# We do this via tmpfiles.d so that it is created by the live system.
-# cat >/usr/lib/tmpfiles.d/brave-browser.conf <<EOF
-# L  /opt/brave.com -  -  -  -  /usr/lib/brave.com
-# EOF
